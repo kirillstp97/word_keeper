9650f10efe5ceaeffe52fdd6e1d78b7f3adc8ad0
kirillstp97 <kirillstp97@gmail.com>
2021-03-12 21:49:13 +0300

    [ADD] components of left side and right side

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <SearchPanel/>
+  <WordList/>
+</template>
+
+<script>
+import SearchPanel from '@/components/SearchPanel'
+import WordList from '@/components/WordList'
+
+export default {
+  name: 'Home',
+  components: {
+    SearchPanel,
+    WordList
+  }
+}
+</script>
