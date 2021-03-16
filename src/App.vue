9af61c898a7abeed41655d04df0d088d3062fb96
kirillstp97 <kirillstp97@gmail.com>
2021-03-17 01:50:32 +0300

    [UPD] moved styles to assets

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
-  <div class="container">
-    <Header/>
-    <router-view/>
-  </div>
+  <Header/>
+  <router-view/>
 </template>
 
 <script>
@@ -15,25 +13,3 @@ export default {
   }
 }
 </script>
-
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap');
-@import url("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css");
-
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-}
-body {
-  font-family: 'Roboto', sans-serif;
-  background: linear-gradient(45deg, #d8d8d8, #fff);
-}
-.container {
-  padding: 10px;
-}
-a {
-  text-decoration: none;
-  color: #fff;
-}
-</style>
