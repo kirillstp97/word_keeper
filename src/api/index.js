e645f548ce31ce7be77cd90dac9ae589d90c4d31
kirillstp97 <kirillstp97@gmail.com>
2021-03-17 00:13:58 +0300

    [ADD] API functions for searching a word by its part and getting their parameters

---
+++ src/api/index.js
@@ -0,0 +1,34 @@
+const API_URL = new URL('https://wordsapiv1.p.rapidapi.com')
+const headers = {
+  'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com',
+  'X-RapidAPI-Key': API_KEY
+}
+
+export async function findWords (pattern) {
+  API_URL.pathname = 'words/'
+  API_URL.search = new URLSearchParams({
+    limit: 10,
+    hasDetails: 'definitions',
+    letterPattern: `^${pattern}`
+  }).toString()
+
+  const data = await fetch(API_URL, { headers })
+  if (data.status !== 200) return []
+
+  const { results } = await data.json()
+  return results.data
+}
+
+export async function getWordParams (_word) {
+  API_URL.pathname = `words/${_word}`
+  const data = await fetch(API_URL, { headers })
+  if (data.status !== 200) return []
+
+  const { word, results, pronunciation, syllables } = await data.json()
+  return {
+    word,
+    ...syllables && { syllables: syllables.list },
+    ...pronunciation && { pronunciation: pronunciation.all },
+    definition: results.map(({ definition }) => definition)
+  }
+}
