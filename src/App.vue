ba70eb75ed1250af78c277d01899a5b8fcfd605b
kirillstp97 <kirillstp97@gmail.com>
2021-03-18 00:07:30 +0300

    [ADD] state & mutation for favorite words, action for fill the var's from local storage

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,17 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex'
 import Header from '@/components/Header'
 
 export default {
   name: 'App',
   components: {
     Header
+  },
+  methods: mapActions(['getLocalSavedData']),
+  created () {
+    this.getLocalSavedData()
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,8 @@ import { findWords, getWordParams } from '@/api'
 
 export default createStore({
   state: {
-    searched_words: []
+    searched_words: [],
+    favorite_words: {}
   },
   actions: {
     async searchByPattern ({ commit }, word) {
@@ -17,11 +18,21 @@ export default createStore({
       commit('updateSearchedList', wordListParams)
       localStorage.setItem('saved_searched_list', JSON.stringify(wordListParams))
     },
+    getLocalSavedData ({ commit }) {
+      const getLocalSearchedWords = JSON.parse(localStorage.getItem('saved_searched_list'))
+      const getLocalFavoriteWords = JSON.parse(localStorage.getItem('saved_favorite_list'))
+      commit('updateSearchedList', getLocalSearchedWords || [])
+      commit('updateFavoriteList', getLocalFavoriteWords || {})
+    },
     }
   },
   mutations: {
     updateSearchedList (state, data) {
       state.searched_words = data
+    },
+    updateFavoriteList (state, data) {
+      state.favorite_words = data
+    },
     }
   }
 })
