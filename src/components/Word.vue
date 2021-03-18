70917c7f732ddb5828cd2e895a00e0cfca01d8e7
kirillstp97 <kirillstp97@gmail.com>
2021-03-19 02:36:25 +0300

    [ADD] used for as a word row template of a wordlist component

---
+++ src/components/Word.vue
@@ -0,0 +1,20 @@
+<template>
+</template>
+
+<script>
+import { mapMutations } from 'vuex'
+
+export default {
+  name: 'Word',
+  props: {
+    word_params: {
+      word: String,
+      definition: String,
+      partOfSpeech: String,
+      isFavorite: Boolean
+  },
+    hasDraggable: Boolean
+  },
+  methods: mapMutations(['toggleFavoriteWords'])
+}
+</script>
