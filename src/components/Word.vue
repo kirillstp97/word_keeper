e4d2a7fa5396c7914a0fff4a5e627cef6fc88e31
kirillstp97 <kirillstp97@gmail.com>
2021-03-20 23:46:40 +0300

    [ADD] page layout, styles

--- src/components/Word.vue
+++ src/components/Word.vue
@@ -1,4 +1,20 @@
 <template>
+  <div class="wordlist-item">
+    <i v-if="hasDraggable" class="wordlist-item__draghandle fas fa-grip-lines"></i>
+    <router-link
+      class="wordlist-item__word"
+      :to="`/parsing/${wordParams.word}`"
+    >
+      {{ wordParams.word }}
+    </router-link>
+    <span class="wordlist-item__part-speech">{{ wordParams.partOfSpeech }}</span>
+    <span class="wordlist-item__desc">{{ wordParams.definition }}</span>
+    <i
+      @click="toggleFavoriteWords(wordParams)"
+      class="wordlist-item__favorite fa-star"
+      :class="wordParams.isFavorite ? 'fas' : 'far'"
+    />
+  </div>
 </template>
 
 <script>
@@ -7,14 +23,44 @@ import { mapActions } from 'vuex'
 export default {
   name: 'Word',
   props: {
-    word_params: {
-      word: String,
-      definition: String,
-      partOfSpeech: String,
-      isFavorite: Boolean
-    },
+    wordParams: Object,
     hasDraggable: Boolean
   },
   methods: mapActions(['toggleFavoriteWords'])
 }
 </script>
+
+<style lang="scss" scoped>
+  .wordlist-item {
+    display: flex;
+    cursor: default;
+    align-items: center;
+    background: #fff;
+    border-radius: 4px;
+    padding: 8px 10px;
+    margin-bottom: 10px;
+
+    & > * {
+      margin: 0 10px;
+      white-space: nowrap;
+    }
+    &__word {
+      font-weight: 600;
+    }
+    &__part-speech {
+      font-style: italic;
+    }
+    &__desc {
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+    &__favorite {
+      color: $theme-color;
+      margin-left: auto;
+      cursor: pointer;
+    }
+    &__draghandle {
+      cursor: alias;
+    }
+  }
+</style>
